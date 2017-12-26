c610dbc8901e6a1fd83cb84ca44751c0b5c977b0
clinzstephen <clinzstephen@gmail.com>
2017-12-26 09:59:52 +0530

    Add files via upload

---
+++ homesecurity.ino
@@ -0,0 +1,36 @@
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(10,9);
+char phone_no[]="+919497612985";
+
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(115200);
+   mySerial.begin(9600);
+   delay(1000);
+   pinMode(7,INPUT);
+   digitalWrite(7,LOW);
+   if(digitalRead(7)==HIGH)
+   {
+      Serial.println("intruders");
+      mySerial.println("AT");     
+      delay(1000);
+      serialdata();
+      mySerial.print("ATD");
+      mySerial.print(phone_no);
+      mySerial.println(";");
+      delay(200000);
+      mySerial.println("ATH"); 
+   }
+}
+void serialdata()
+{
+   while(mySerial.available()!=0)
+   {  Serial.write(mySerial.read());
+   //Serial.println("hello world");
+}
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
